a4549f038832b135673c0f8c6726788101f3a5f2
lionkingofnorth <luoyang19922@gmail.com>
2020-06-14 16:33:38 +0800

    [ first week update ] note.md update

--- Week01/26.remove-duplicates-from-sorted-array.go
+++ Week01/26.remove-duplicates-from-sorted-array.go
@@ -66,7 +66,6 @@ func removeDuplicates(nums []int) int {
     if len(nums) == 0 {
         return 0
     }
-    
     out := 1
     for i := 1; i < len(nums); i++ {
         if nums[i-1] != nums[i] {
